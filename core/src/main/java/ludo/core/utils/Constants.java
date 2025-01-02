d5a6f6539c85fcaeb44b128728b974c6d42ecb03
Juvraj Singh Banwait <10777524@polimi.it>
2025-01-02 17:29:54 +0100

    Utils classes

---
+++ core/src/main/java/ludo/core/utils/Constants.java
@@ -0,0 +1,24 @@
+package ludo.core.utils;
+public class Constants {
+    // Board dimensions
+    public static final int BOARD_SIZE = 52;
+    public static final int HOME_COLUMN_SIZE = 6;
+
+    // Player colors
+    public static final String RED = "RED";
+    public static final String GREEN = "GREEN";
+    public static final String BLUE = "BLUE";
+    public static final String YELLOW = "YELLOW";
+
+    // Screen dimensions
+    public static final int VIEWPORT_WIDTH = 800;
+    public static final int VIEWPORT_HEIGHT = 600;
+
+    // UI constants
+    public static final float BUTTON_PADDING = 10f;
+
+    // Game constants
+    public static final int MAX_PLAYERS = 4;
+    public static final int PAWNS_PER_PLAYER = 4;
+    public static final int DICE_MAX = 6;
+}
