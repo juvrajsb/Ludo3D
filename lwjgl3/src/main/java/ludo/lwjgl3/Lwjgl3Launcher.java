6b10aa721f46151132cc143ad5fbe7490b2298fd
Juvraj Singh Banwait <10777524@polimi.it>
2025-01-20 00:08:48 +0100

    Desktop Launcher fix

--- lwjgl3/src/main/java/ludo/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/ludo/lwjgl3/Lwjgl3Launcher.java
@@ -1,25 +1,43 @@
-package main.java.ludo.lwjgl3;
+package ludo.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import ludo.client.LudoGame;
 
 public class Lwjgl3Launcher {
+    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_HEIGHT = 600;
+    private static final String GAME_TITLE = "Ludo Game";
+
     public static void main(String[] args) {
-        createApplication();
+        try {
+            if (StartupHelper.startNewJvmIfRequired()) return;
+            createApplication();
+        } catch (Exception e) {
+            System.err.println("Error launching game: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new LudoGame(), getDefaultConfiguration());
+        try {
+            return new Lwjgl3Application(new LudoGame(), getDefaultConfiguration());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create application", e);
+        }
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("Ludo");
+
+        configuration.setTitle(GAME_TITLE);
+        configuration.setWindowedMode(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        configuration.setWindowIcon("assets/icon.png");
         configuration.useVsync(true);
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
-        configuration.setWindowedMode(800, 600);
-        configuration.setWindowIcon("ui/icon.png", "ui/icon.png", "ui/icon.png", "ui/icon.png");
+        configuration.setForegroundFPS(60);
+        configuration.setResizable(true);
+
         return configuration;
     }
 }
