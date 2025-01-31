0c9149060193a81995d259a4c5d81cac66e71f4e
Juvraj Singh Banwait <10777524@polimi.it>
2025-01-31 19:49:01 +0100

    Update Event.java

--- core/src/main/java/ludo/core/events/Event.java
+++ core/src/main/java/ludo/core/events/Event.java
@@ -3,8 +3,11 @@ package ludo.core.events;
 import ludo.core.network.Connection;
 import ludo.core.network.NetworkMessage;
 
+import java.io.Serializable;
 
-public abstract class Event implements NetworkMessage {
+
+public abstract class Event implements NetworkMessage, Serializable {
+    private static final long serialVersionUID = 1L;
     protected String ID;
     protected transient Connection connection;
 
