02535659b71b6ec0f4d9f94c981b9daa50be6aa7
Juvraj Singh Banwait <10777524@polimi.it>
2025-01-31 19:48:38 +0100

    Update TurnChangeEvent.java

--- core/src/main/java/ludo/core/events/serverToClient/TurnChangeEvent.java
+++ core/src/main/java/ludo/core/events/serverToClient/TurnChangeEvent.java
@@ -1,21 +1,22 @@
 // PlayerJoinedEvent.java
 package ludo.core.events.serverToClient;
 
-import ludo.core.entities.Player;
 import ludo.core.events.Event;
-
+import ludo.core.network.NetworkMessage;// Fix TurnChangeEvent.java
 public class TurnChangeEvent extends Event {
+    private final String currentPlayer;
 
-    public TurnChangeEvent(Player player) {
+    public TurnChangeEvent(String currentPlayer) {
         super("TURN_CHANGE");
+        this.currentPlayer = currentPlayer;
+    }
+
+    public String getCurrentPlayer() {
+        return currentPlayer;
     }
 
     @Override
     public void process() {
         // Will be processed by client handler
-    }
-
-    public String getCurrentPlayer() {
-        return null;
     }
 }
