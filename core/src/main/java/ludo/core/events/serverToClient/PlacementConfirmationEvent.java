4ae11a0659adb9f4a599927ea00937c0a308fc8d
Juvraj Singh Banwait <10777524@polimi.it>
2025-02-06 12:14:30 +0100

    commented out unused classes

--- core/src/main/java/ludo/core/events/serverToClient/PlacementConfirmationEvent.java
+++ core/src/main/java/ludo/core/events/serverToClient/PlacementConfirmationEvent.java
@@ -1,15 +1,15 @@
-package ludo.core.events.serverToClient;
-
-import ludo.core.events.Event;
-
-public class PlacementConfirmationEvent extends Event {
-
-    public PlacementConfirmationEvent(int gainedPoints) {
-        super("PLACEMENT_CONFIRMATION");
-    }
-
-    @Override
-    public void process() {
-
-    }
-}
+//package ludo.core.events.serverToClient;
+//
+//import ludo.core.events.Event;
+//
+//public class PlacementConfirmationEvent extends Event {
+//
+//    public PlacementConfirmationEvent(int gainedPoints) {
+//        super("PLACEMENT_CONFIRMATION");
+//    }
+//
+//    @Override
+//    public void process() {
+//
+//    }
+//}
