5412a16b383cc21e31387f9a01dcb4e0a28466ab
Juvraj Singh Banwait <10777524@polimi.it>
2025-05-28 18:11:32 +0200

    Update MessageListener.java

--- core/src/main/java/ludo/core/network/MessageListener.java
+++ core/src/main/java/ludo/core/network/MessageListener.java
@@ -1,5 +1,8 @@
 package ludo.core.network;
 
+/**
+ * Interface for classes that want to receive network messages.
+ */
 public interface MessageListener {
     void onMessageReceived(NetworkMessage message);
     void onConnectionError(Exception e);
