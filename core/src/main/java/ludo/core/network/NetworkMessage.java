8b5ba0345d7a6d30a0a89c046715f61f5bd40bec
Juvraj Singh Banwait <10777524@polimi.it>
2025-05-28 18:11:40 +0200

    Update NetworkMessage.java

--- core/src/main/java/ludo/core/network/NetworkMessage.java
+++ core/src/main/java/ludo/core/network/NetworkMessage.java
@@ -2,6 +2,10 @@ package ludo.core.network;
 
 import java.io.*;
 
+/**
+ * Interface for all network messages in the game.
+ * Messages are used for communication between client and server.
+ */
 public interface NetworkMessage extends Serializable {
     /**
      * Gets the type identifier for this message
