dce3e639bfeaf2a97fba2b2db35325e858989ac4
Juvraj Singh Banwait <10777524@polimi.it>
2025-06-03 20:05:41 +0200

    Update Constants.java

--- core/src/main/java/ludo/core/utils/Constants.java
+++ core/src/main/java/ludo/core/utils/Constants.java
@@ -13,15 +13,15 @@ public class Constants {
     public static final String YELLOW = "YELLOW";
 
     // Screen dimensions
-    public static final int VIEWPORT_WIDTH = 800;
-    public static final int VIEWPORT_HEIGHT = 600;
+//    public static final int VIEWPORT_WIDTH = 800;
+//    public static final int VIEWPORT_HEIGHT = 600;
 
     // UI constants
-    public static final float BUTTON_PADDING = 10f;
+//    public static final float BUTTON_PADDING = 10f;
 
     // Game constants
     public static final int MAX_PLAYERS = 4;
-    public static final int MIN_PLAYERS = 2;
+//    public static final int MIN_PLAYERS = 2;
     public static final int PAWNS_PER_PLAYER = 4;
-    public static final int DICE_MAX = 6;
+//    public static final int DICE_MAX = 6;
 }
