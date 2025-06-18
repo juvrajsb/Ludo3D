ee02dc048984447d8fc14a8e672ac34f012e5ad7
Juvraj Singh Banwait <10777524@polimi.it>
2025-06-18 18:14:30 +0200

    Update EventReceiver.java

--- server/src/main/java/ludo/server/networking/EventReceiver.java
+++ server/src/main/java/ludo/server/networking/EventReceiver.java
@@ -5,12 +5,6 @@ import ludo.core.events.Event;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * Class that receives the Events from the socket through a Queue
- * and calls their associated handlers.
- *
- * @see EventListener
- */
 public class EventReceiver implements Runnable {
     private final Queue<Event> eventsQueue;
     private boolean stop = false;
@@ -19,10 +13,6 @@ public class EventReceiver implements Runnable {
         this.eventsQueue = new LinkedList<>();
     }
 
-    /**
-     * Method that allows the reception of events. The thread sleeps until an Event arrives: when an Event arrives, the
-     * EventReceiver is woken up and can receive the event by reading it from the {@code eventsQueue}.
-     */
     @Override
     public void run() {
         while(true) {
@@ -42,7 +32,6 @@ public class EventReceiver implements Runnable {
             synchronized (eventsQueue) {
                 receivedEvent = eventsQueue.remove();
             }
-
             receivedEvent.process();
         }
     }
